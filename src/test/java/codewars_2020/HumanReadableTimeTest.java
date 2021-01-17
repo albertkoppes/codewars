e272633cfcbd7813cf25b3e4f9c8d8e013dd36be
albertkoppes <44414365+albertkoppes@users.noreply.github.com>
2021-01-17 20:02:00 +0100

    First commit

--- src/test/java/codewars_2020/HumanReadableTimeTest.java
+++ src/test/java/codewars_2020/HumanReadableTimeTest.java
@@ -2,17 +2,16 @@ package codewars_2020;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class HumanReadableTimeTest
 {
     @Test
     public void Tests() {
-        Assertions.assertEquals("makeReadable(5)", "00:00:05", HumanReadableTime.makeReadable(5));
-        assertEquals("makeReadable(60)", "00:01:00", HumanReadableTime.makeReadable(60));
-        assertEquals("makeReadable(86399)", "23:59:59", HumanReadableTime.makeReadable(86399));
-        assertEquals("makeReadable(359999)", "99:59:59", HumanReadableTime.makeReadable(359999));
+        assertEquals("00:00:05", HumanReadableTime.makeReadable(5), "makeReadable(5)");
+        assertEquals( "00:01:00", HumanReadableTime.makeReadable(60), "makeReadable(60)");
+        assertEquals( "23:59:59", HumanReadableTime.makeReadable(86399), "makeReadable(86399)");
+        assertEquals( "99:59:59", HumanReadableTime.makeReadable(359999), "makeReadable(359999)");
     }
 
 }
